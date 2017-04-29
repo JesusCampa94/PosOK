9ae222ce94ba4470edabeb8a006d9a6fc9c880be
Jesus <jesuscampa94@gmail.com>
2017-04-29 17:24:14 +0200

    Avanzando en nueva entrada
    Añade y quita tarjetas e inputs. Falta arreglar el cargarFoto() e
    implementar enviarFotos().

--- js/nueva-entrada.js
+++ js/nueva-entrada.js
@@ -1,3 +1,6 @@
+//Contador de fotos (autoincremento)
+let fotos = 0;
+
 //Pinta el borde inferior de nueva entrada en la barra de navegacion
 ! function estoyEnNuevaEntrada()
 {
@@ -49,38 +52,75 @@ function crearEntrada(form)
 	return false;
 }
 
-//Inserta la carta para cargar una imagen nueva
-function insertarCarta()
+//Inserta una nueva tarjeta en el DOM, ademas de un input tipo file y un textarea para el form oculto
+function nuevaFoto()
 {
-	let html = '', divGaleria = document.querySelector('.galeria'), botonInserta = document.getElementById('nueva-foto');
-	let article = document.createElement('article');
+	//Punteros
+	let divGaleria = document.querySelector('.galeria'),
+		botonInsertar = document.getElementById('nueva-foto'),
+		formFotos = document.getElementById('form-fotos');
+	
+	//Aumentar contador fotos
+	fotos++;
 
-	html += ' 	<div class="no-grow">';
-	html += '		<figure>';
-	html += '			<img src="img/com/image.png" height="300" width="300" alt="" onclick="this.parentNode.querySelector(\'[type=file]\').click();">';
-	html += '         <input type="file" accept="image/*" onchange="mostrarFoto(this);">';
-	html += '		</figure>';
-	html += '	</div>';
-	html += '	<a href="#" class="boton"><i class="material-icons">&#xE2C6;</i>Seleccionar fichero</a>';
-	html += '	<a href="#" class="boton peligro"><i class="material-icons">&#xE872;</i>Eliminar fichero</a>';
-	html += '	<div>';
-	html += '		<textarea form="form-nueva-entrada" name="desc-img-3" id="desc-img-3" cols="30" rows="6" placeholder="Escriba una descripción..." required></textarea>';
-	html += '	</div>';
+	//Nuevos elementos
+	let article = document.createElement('article'),
+		html = '',
+		codigoInput = '<input type="file" accept="image/*"  id="input-' + fotos +'" onchange="cargarFoto(this);">',
+		queryDelSelector = '#form-fotos>input:nth-of-type(' + fotos + ')';
 
+	// console.log('Fotos: ' + fotos + ' selector: document.querySelector(\'' + queryDelSelector + '\')');
+
+	formFotos.innerHTML += codigoInput;
+
+	html += '<div class="no-grow">';
+	html += '	<figure>';
+	html += '		<img src="img/com/image.png" height="300" width="300" alt="" onclick="document.querySelector(\'' + queryDelSelector + '\').click();">';
+	html += '	</figure>';
+	html += '	<textarea form="form-fotos" name="desc-img-3" id="desc-img-3" cols="30" rows="6" placeholder="Escriba una descripción..." required></textarea>';
+	html += '</div>';
+	html += '<div>';
+	html += '	<a onclick="document.querySelector(\'' + queryDelSelector + '\').click();" class="boton"><i class="material-icons">&#xE2C6;</i>Seleccionar fichero</a>';
+	html += '	<a onclick="quitarFoto(this.parentNode.parentNode);" class="boton peligro"><i class="material-icons">&#xE872;</i>Suprimir</a>';
+	html += '</div>';
+
+	article.id = 'foto-' + fotos;
 	article.innerHTML = html;
-	divGaleria.insertBefore(article, botonInserta);
+	divGaleria.insertBefore(article, botonInsertar);
 }
 
-//muestra la foto a insertar
-function mostrarFoto(input)
+
+//Quita una foto
+function quitarFoto(article)
+{
+	let inputAEliminar = article.id.replace('foto-', 'input-');
+
+	//Eliminamos el article y su input
+	article.remove();
+	document.getElementById(inputAEliminar).remove();
+}
+
+
+//Carga una foto nueva
+function cargarFoto(input)
 {
 	let fr = new FileReader();
 
 	fr.onload = function()
 	{
+		//CAMBIAR!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 		input.parentNode.querySelector('img').src = fr.result;
 		input.parentNode.querySelector('img').alt = input.files[0].name;
 	};
 
 	fr.readAsDataURL(input.files[0]);
+}
+
+
+//Envia las fotos de la entrada recien creada
+function enviarFotos(form)
+{
+
+
+	return false;
 }
